1889ec98ec36083a3cf8dbd4d16552ffcc23ce49
edward-ganuelas <epganuelas@gmail.com>
2024-07-13 12:09:54 -0400

    add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,14 @@
+import '../src/assets/style.css'
+/** @type { import('@storybook/vue3').Preview } */
+const preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;

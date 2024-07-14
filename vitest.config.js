531030121318ba7932ae8d3fae275d62c2844584
Edward Paulo Ganuelas <epganuelas@gmail.com>
2024-07-14 18:43:59 -0400

    add coverage

--- vitest.config.js
+++ vitest.config.js
@@ -11,7 +11,8 @@ export default mergeConfig(
       root: fileURLToPath(new URL('./', import.meta.url)),
       watch: false,
       coverage: {
-        provider: 'istanbul'
+        provider: 'istanbul',
+        exclude: ['src/stories/**', 'components/**.stories.js']
       }
     }
   })

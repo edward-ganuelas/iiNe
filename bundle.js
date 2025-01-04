81e0c7fec110abc8ea0822dc8ea0c7640307fe62
edward-ganuelas <epganuelas@gmail.com>
2025-01-03 19:21:30 -0500

    rename

--- bundle.js
+++ bundle.js
@@ -4,12 +4,12 @@ import IiButton from '@/components/IiButton.vue'
 import IiHeaders from '@/components/IiHeaders.vue'
 import IiInput from '@/components/IiInput.vue'
 import IiList from '@/components/IiList.vue'
-import iiParagraph from '@/components/iiParagraph.vue'
+import IiParagraph from '@/components/iiParagraph.vue'
 
 export {
     IiButton,
     IiHeaders,
     IiInput,
     IiList,
-    iiParagraph
+    IiParagraph
 }
